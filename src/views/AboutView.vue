369599d0b446f5f1f524aa23a4dcf20b40bfa4f8
Peng Jin <peng.jin@symbio.com>
2022-11-23 17:22:22 +0800

    ok

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <h5>{{counterStore.count}}</h5>
+      <button @click="counterStore.increment">{{counterStore.doubleCount}}</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { useCounterStore } from "../stores/counter.ts";
+  const counterStore = useCounterStore()
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -10,6 +19,7 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: space-between;
   }
 }
 </style>
