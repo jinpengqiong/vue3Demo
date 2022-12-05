6ff234217ee9e03b977616529052d310a2dc5b70
Peng Jin <peng.jin@symbio.com>
2022-12-05 08:49:55 +0800

    ok

---
+++ src/views/Playground.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>昵称: {{person.name}}</div>
+    <div>年龄: {{person.age}}</div>
+    <!-- <div>昵称: {{name}}</div>
+    <div>年龄: {{age}}</div> -->
+    <button @click="update">按钮</button>
+  </div>
+</template>
+
+<script>
+  import { ref, watch, reactive } from 'vue';
+  export default {
+    setup() {
+      const name = ref('橙某人');
+      const age = ref(18);
+      const person = reactive({
+        name: '橙某人',
+        age: 18
+      });
+
+      watch([() => person.name, () => person.age], (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      });
+
+      function update() {
+        person.value.name = 'YDYDYDQ';
+        // person.age = 20;
+      }
+
+      return { person, name, age, update }
+    }
+  }
+</script>
