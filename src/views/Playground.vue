01755244126e1a875f903027a6c612936d28cc4c
Peng Jin <peng.jin@symbio.com>
2022-12-05 09:40:05 +0800

    watchEffect

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,34 +1,31 @@
 <template>
   <div>
-    <div>昵称: {{person.name}}</div>
-    <div>年龄: {{person.age}}</div>
-    <!-- <div>昵称: {{name}}</div>
-    <div>年龄: {{age}}</div> -->
-    <button @click="update">按钮</button>
+    <button @click="update1">按钮1</button>
+    <button @click="update2">按钮2</button>
   </div>
 </template>
 
 <script>
-  import { ref, watch, reactive } from 'vue';
+  import { ref, watchEffect } from 'vue';
   export default {
     setup() {
       const name = ref('橙某人');
       const age = ref(18);
-      const person = reactive({
-        name: '橙某人',
-        age: 18
+
+      watchEffect(() => {
+        console.log('**********');
+        console.log(name.value);
+        console.log(age.value);
       });
 
-      watch([() => person.name, () => person.age], (newValue, oldValue) => {
-        console.log(newValue, oldValue);
-      });
-
-      function update() {
-        person.value.name = 'YDYDYDQ';
-        // person.age = 20;
+      function update1() {
+        name.value = 'YDYDYDQ';
+      }
+      function update2() {
+        age.value = 20;
       }
 
-      return { person, name, age, update }
+      return { update1, update2 }
     }
   }
 </script>
