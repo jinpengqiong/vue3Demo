2ecb012934570aa57faa8fa701e727769333e545
Peng Jin <peng.jin@symbio.com>
2022-12-07 09:22:05 +0800

    teleport

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,26 +1,36 @@
 <template>
-  <div>{{ reactivePerson }}</div>
-  <div>{{ toRawPerson }}</div>
-  <button @click="update">按钮</button>
+  <div id="box">
+    <p>id选择器</p>
+  </div>
+  <div class="box">
+    <p>class选择器</p>
+  </div>
+  <div data-box>
+    <p>class选择器</p>
+  </div>
+  <!--teleport-->
+  <teleport :to="target">
+    <h2>橙某人</h2>
+  </teleport>
+  <div>
+    <button @click="update('body')">传送到body中</button>
+    <button @click="update('#box')">传送到id选择器中</button>
+    <button @click="update('.box')">传送到class选择器中</button>
+    <button @click="update('[data-box]')">传送到属性选择器中</button>
+  </div>
 </template>
-<script>
-  import { reactive, toRaw } from 'vue';
 
+<script>
+  import { ref } from 'vue';
   export default {
     setup() {
-      const person = { name: '橙某人', age: 18 };
-      const reactivePerson = reactive(person);
-      const toRawPerson = toRaw(reactivePerson);
+      const target = ref('#app'); // 初始化渲染的位置
 
-      console.log(person === toRawPerson); // true
-
-      function update() {
-        toRawPerson.name = 'YDYDYDQ';
-        reactivePerson.name = '212123123'
-        console.log(person, reactivePerson, toRawPerson);
+      function update(selector) {
+        target.value = selector;
       }
 
-      return { reactivePerson, toRawPerson, update };
+      return { target, update }
     }
   }
 </script>
